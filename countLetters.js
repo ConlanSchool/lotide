8f092315c4194e98e0fc2f5d7ef1e2ba2a444932
ConlanSchool <conlan.school@gmail.com>
2023-01-30 14:50:27 -0800

    Counts the letters in a sentence and returns them as an object.

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+//Counts letters of a sentence in an object
+const countLetters = function(sentence) {
+  let countObj = {};
+
+  //Loops through the input sentence
+  for (let character of sentence) {
+    //checks if it is already in the object
+    if (countObj[character]) {
+      countObj[character] += 1
+
+    } else {
+      countObj[character] = 1
+    }
+  }
+  return countObj;
+};
+
+//TEST CASE 
+let test1 = countLetters('aaabbbcccd');
+
+assertEqual(test1['a'], 3);
+assertEqual(test1['d'], 1);
