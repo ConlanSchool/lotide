35b08f02268f876bf055d809e0f2640b7e30976b
ConlanSchool <conlan.school@gmail.com>
2023-01-30 15:53:18 -0800

    finds the key of a given value if it is in the object

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+const findKeyByValue = function (object, value) {
+
+  //Cycle through the keys
+  for (let key in object) {
+    
+    //checks if the value of the key matches the value given
+    if (object[key] === value) {
+
+      //return the name the the key(not its value)
+      return key;
+    }
+  }
+};
+
+
+
+//TEST CASE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
