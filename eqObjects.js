58befa53fc1a4e1bd52ced14bc7fb610cbc681d1
ConlanSchool <conlan.school@gmail.com>
2023-02-18 12:59:58 -0800

    Clean up redundant functions

--- eqObjects.js
+++ eqObjects.js
@@ -1,26 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-
-const eqArrays = function (arr1, arr2) {
-  //First compare length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  //Loop and compare each element
-  for (let i = 0 ; i < arr1.length ; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays')
+const assertEqual = require('./assertEqual')
 
 
 
--- map.js
+++ map.js
@@ -1,25 +1,6 @@
-const eqArrays = function (arr1, arr2) {
-  //First compare length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  //Loop and compare each element
-  for (let i = 0 ; i < arr1.length ; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
 
-//Prints the result message
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Equal`);
-  } else {
-    console.log(`🛑🛑🛑 Not equal`);
-  }
-};
 
 
 
